ee57a4b1fef77b841564ff1571b0c3942a617ec2
BridgeQ <441828384@qq.com>
2021-07-18 06:42:51 +0800

    UI 2.0

---
+++ Assets/scripts/UI/DetailedPanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetailedPanel : MonoBehaviour
+{
+    public Button Scent1Button;
+    public Button Scent2Button;
+    public Button Scent3Button;
+    public Button Scent4Button;
+
+    public Animator DetailedPanelAnimator;
+
+    public Button closeButton;
+
+    public GameObject MiniDetectMenu;
+
+    void Start()
+    {
+        Scent1Button.onClick.AddListener(call:() => 
+        {
+            DetailedPanelAnimator.SetTrigger(name:"Fadein");
+            MiniDetectMenu.SetActive(false);
+        });
+        Scent2Button.onClick.AddListener(call:() => 
+        {
+            DetailedPanelAnimator.SetTrigger(name:"Fadein");
+            MiniDetectMenu.SetActive(false);
+        });
+        Scent3Button.onClick.AddListener(call:() => 
+        {
+            DetailedPanelAnimator.SetTrigger(name:"Fadein");
+            MiniDetectMenu.SetActive(false);
+        });
+        Scent4Button.onClick.AddListener(call:() => 
+        {
+            DetailedPanelAnimator.SetTrigger(name:"Fadein");
+            MiniDetectMenu.SetActive(false);
+        });
+        closeButton.onClick.AddListener(call:() => 
+        {
+            DetailedPanelAnimator.SetTrigger(name:"Fadeout");
+            MiniDetectMenu.SetActive(true);
+        });
+    }
+}
--- Assets/scripts/UI/WOD.cs
+++ Assets/scripts/UI/WOD.cs
@@ -4,26 +4,47 @@ using UnityEngine;
 
 public class WOD : MonoBehaviour
 {
-    public int TapE;
     public GameObject WODon;
     public GameObject WODoff;
     public GameObject WODemitting;
+    public GameObject MiniDetectMenu;
+    public GameObject Detecting;
+    public GameObject Emitting;
+    public GameObject DetailedPanel;
     void Update()
     {
+        //Detect the scent
         if (Input.GetKeyDown(KeyCode.E))
-        {
-            TapE++;
-        }
-        if (TapE == 1)
         {
             WODoff.SetActive(false);
             WODemitting.SetActive(false);
             WODon.SetActive(true);
+            MiniDetectMenu.SetActive(true);
+            Detecting.SetActive(true);
+            Emitting.SetActive(false);
+            DetailedPanel.SetActive(true);
         }
-        else
+        //Emit the scent
+        if (Input.GetKeyDown(KeyCode.F))
         {
+            WODoff.SetActive(false);
+            WODemitting.SetActive(true);
             WODon.SetActive(false);
-            TapE = 0;
+            MiniDetectMenu.SetActive(false);
+            Detecting.SetActive(false);
+            Emitting.SetActive(true);
+            DetailedPanel.SetActive(false);
+        }
+        //Turn off the WOD
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            WODoff.SetActive(true);
+            WODemitting.SetActive(false);
+            WODon.SetActive(false);
+            MiniDetectMenu.SetActive(false);
+            Detecting.SetActive(false);
+            Emitting.SetActive(false);
+            DetailedPanel.SetActive(false);
         }
     }
 }
